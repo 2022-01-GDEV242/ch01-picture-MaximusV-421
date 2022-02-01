31a67ddc5574c006682867c5b0c025770d0580a4
MaximusV <98302019+MaximusV-421@users.noreply.github.com>
2022-01-31 21:59:00 -0500

    Complete Picture With Methods
    This picture is a simple home with various methods for changing the setting.

--- Picture.java
+++ Picture.java
@@ -1,21 +1,24 @@
 /**
- * This class represents a simple picture. You can draw the picture using
- * the draw method. But wait, there's more: being an electronic picture, it
- * can be changed. You can set it to black-and-white display and back to
- * colors (only after it's been drawn, of course).
+ * This class represents a basic home with methods such as setting and rising the sun
+ * and also allows the user to move the person back and forth once the sun is set.
  *
- * This class was written as an early example for teaching Java with BlueJ.
- * 
- * @author  Michael Kšlling and David J. Barnes
- * @version 2016.02.29
+ * @author Maximus Vancaneghem
+ * @version 2022.01.31
  */
 public class Picture
 {
     private Square wall;
     private Square window;
+    private Triangle car;
+    private Square door;
+    private Circle door2;
     private Triangle roof;
     private Circle sun;
+    private Circle wheel;
+    private Circle wheel2;   
     private Person person;
+    private Square chimney;
+    private Square chimney2;
     private boolean drawn;
 
     /**
@@ -25,9 +28,16 @@ public class Picture
     {
         wall = new Square();
         window = new Square();
+        door = new Square();
+        door2 = new Circle();
+        car = new Triangle();
         roof = new Triangle();  
         sun = new Circle();
+        wheel = new Circle();
+        wheel2 = new Circle();
         person = new Person();
+        chimney = new Square();
+        chimney2 = new Square();
         drawn = false;
     }
 
@@ -37,6 +47,18 @@ public class Picture
     public void draw()
     {
         if(!drawn) {
+            chimney.changeColor("black");
+            chimney.changeSize(20);
+            chimney.moveVertical(-30);
+            chimney.moveHorizontal(-45);
+            chimney.makeVisible();
+            
+            chimney2.changeColor("black");
+            chimney2.changeSize(30);
+            chimney2.moveHorizontal(-50);
+            chimney2.moveVertical(-20);
+            chimney2.makeVisible();
+            
             wall.moveHorizontal(-140);
             wall.moveVertical(20);
             wall.changeSize(120);
@@ -47,6 +69,18 @@ public class Picture
             window.moveVertical(40);
             window.changeSize(40);
             window.makeVisible();
+            
+            door.changeColor("black");
+            door.moveVertical(110);
+            door.moveHorizontal(-60);
+            door.changeSize(30);
+            door.makeVisible();
+            
+            door2.changeColor("black");
+            door2.moveVertical(130);
+            door2.moveHorizontal(20);
+            door2.changeSize(30);
+            door2.makeVisible();
     
             roof.changeSize(60, 180);
             roof.moveHorizontal(20);
@@ -54,13 +88,33 @@ public class Picture
             roof.makeVisible();
     
             sun.changeColor("yellow");
-            sun.moveHorizontal(100);
+            sun.moveHorizontal(-200);
             sun.moveVertical(-40);
             sun.changeSize(80);
             sun.makeVisible();
             
             person.moveVertical(130);
+            person.moveHorizontal(-130);
             person.makeVisible();
+            
+            wheel.changeColor("black");
+            wheel.changeSize(20);
+            wheel.moveHorizontal(80);
+            wheel.moveVertical(180);
+            wheel.makeVisible();
+            
+            wheel2.changeColor("black");
+            wheel2.changeSize(20);
+            wheel2.moveHorizontal(180);
+            wheel2.moveVertical(180);
+            wheel2.makeVisible();
+            
+            car.changeColor("blue");
+            car.changeSize(30, 180);
+            car.moveHorizontal(160);
+            car.moveVertical(100);
+            car.makeVisible();
+            
             drawn = true;
         }
     }
@@ -74,6 +128,13 @@ public class Picture
         window.changeColor("white");
         roof.changeColor("black");
         sun.changeColor("black");
+        wheel.changeColor("black");
+        wheel2.changeColor("black");
+        car.changeColor("black");
+        door.changeColor("white");
+        door2.changeColor("white");
+        chimney.changeColor("black");
+        chimney2.changeColor("black");
     }
 
     /**
@@ -85,30 +146,46 @@ public class Picture
         window.changeColor("black");
         roof.changeColor("green");
         sun.changeColor("yellow");
+        wheel.changeColor("black");
+        wheel2.changeColor("black");
+        car.changeColor("blue");
+        door.changeColor("black");
+        door2.changeColor("black");
+        chimney.changeColor("white");
+        chimney2.changeColor("white");
     }
     
     /**
-     * Change this picture to make the sunset 
+     * Change this picture to make the sun set 
      */
-    public void sunset()
+    public void sunSet()
     {
         sun.slowMoveVertical(250);
         person.slowMoveVertical(-65);
     }
     
     /**
-     * Change this picture to make the sunrise
+     * Change this picture to make the sun rise
      */
-    public void sunrise()
+    public void sunRise()
     {
         sun.slowMoveVertical(-250);
+        person.slowMoveVertical(65);
     }
     
     /**
-     * Change this picture to make a person walk up to the house
+     * Change this picture to make a person walk to left
      */
     public void walk()
     {
-        person.slowMoveVertical(-65);
+        person.slowMoveHorizontal(-65);
+    }
+    
+    /**
+     * Change this picture to make a person walk to their previous position
+     */
+    public void walkBack()
+    {
+        person.slowMoveHorizontal(65);
     }
 }
